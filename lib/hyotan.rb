c0db8f6e899d1354f18f30a4ae9db7634d52ce80
Hisaki Akaza <TheRed@users.noreply.github.com>
2017-03-13 06:19:27 +0900

    Add test for cabocha -f3 のXML出力

--- lib/hyotan.rb
+++ lib/hyotan.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 require 'hyotan/version'
+require 'cabocha'
 
 module Hyotan
-  # Your code goes here...
+  class MyCaboCha
+    def run(sentence)
+      parser = CaboCha::Parser.new
+      tree = parser.parse(sentence)
+      output = tree.toString(CaboCha::FORMAT_XML).force_encoding('utf-8').encode('utf-8')
+      output
+    end
+  end
 end
--- spec/hyotan_spec.rb
+++ spec/hyotan_spec.rb
@@ -6,7 +6,24 @@ RSpec.describe Hyotan do
     expect(Hyotan::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'cabocha -f3 のXML出力' do
+    output = <<~EOS
+    <sentence>
+     <chunk id="0" link="2" rel="D" score="-0.744433" head="0" func="1">
+      <tok id="0" feature="名詞,代名詞,一般,*,*,*,それ,ソレ,ソレ">それ</tok>
+      <tok id="1" feature="助詞,係助詞,*,*,*,*,は,ハ,ワ">は</tok>
+     </chunk>
+     <chunk id="1" link="2" rel="D" score="-0.744433" head="2" func="3">
+      <tok id="2" feature="名詞,一般,*,*,*,*,世間,セケン,セケン">世間</tok>
+      <tok id="3" feature="助詞,格助詞,一般,*,*,*,が,ガ,ガ">が</tok>
+      <tok id="4" feature="記号,読点,*,*,*,*,、,、,、">、</tok>
+     </chunk>
+     <chunk id="2" link="-1" rel="D" score="0.000000" head="5" func="6">
+      <tok id="5" feature="動詞,自立,*,*,五段・サ行,未然形,ゆるす,ユルサ,ユルサ">ゆるさ</tok>
+      <tok id="6" feature="助動詞,*,*,*,特殊・ナイ,基本形,ない,ナイ,ナイ">ない</tok>
+     </chunk>
+    </sentence>
+    EOS
+    expect(Hyotan::MyCaboCha.new.run('それは世間が、ゆるさない')).to eq(output)
   end
 end
